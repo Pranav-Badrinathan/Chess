3514aabe6ee1cf7add470fa18cd905154fdb4c5c
Pranav Badrinathan <pranav.badrinathan@gmail.com>
2020-01-05 11:47:04 -0400

    Added movement for the Knight and the Queen

--- src/main/java/game/chess/pieces/Queen.java
+++ src/main/java/game/chess/pieces/Queen.java
@@ -2,11 +2,24 @@ package game.chess.pieces;
 
 import game.chess.enums.ChessColor;
 import game.chess.enums.PieceType;
+import game.chess.gui.Tile;
 
 public class Queen extends Piece
 {
 	public Queen(ChessColor pieceColor)
 	{
 		super(PieceType.QUEEN, pieceColor);
+	}
+	
+	@Override
+	public boolean isValidMove(Tile currentTile, Tile targetTile)
+	{
+		Rook r = new Rook(pieceColor);
+		Bishop b = new Bishop(pieceColor);
+		
+		if(r.isValidMove(currentTile, targetTile) || b.isValidMove(currentTile, targetTile))
+			return true;
+		else
+			return false;
 	}
 }
